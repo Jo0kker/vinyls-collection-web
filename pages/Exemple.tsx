d708d23ad3d02730315569f97750c8d463a7a14b
Vcna-0 <ncav@protonmail.com>
2023-04-07 17:25:15 +0200

    feat:install React Query

---
+++ pages/Exemple.tsx
@@ -0,0 +1,60 @@
+import {Cookies} from "react-cookie";
+import {useQuery} from "@tanstack/react-query";
+import axiosApiInstance from "../services/interceptorService";
+import {AxiosResponse} from "axios";
+import Lottie from "lottie-react";
+import loading from "@assets/lottieJson/88944-vinyl-loading.json";
+import {Layout} from "@components/Layout";
+import {Toaster} from "react-hot-toast";
+import Banner from "@components/Banner";
+import NextNProgress from "nextjs-progressbar";
+import {FooterPerso} from "@components/FooterPerso";
+import {QueryClient} from "@tanstack/react-query";
+import {FunctionComponent, PropsWithChildren} from "react";
+import {AppProps} from "next/app";
+
+const queryClient = new QueryClient();
+
+// type Props = PropsWithChildren<{
+//     Component: any;
+//     pageProps: any;
+// }>;
+
+export const Exemple: FunctionComponent<AppProps> = ({ Component, pageProps}) => {
+
+    // token in cookie
+    const cookie = new Cookies();
+    const token = cookie.get("token");
+
+    const { isLoading, data: userData } = useQuery({
+        queryKey: ['user'],
+        queryFn: () => axiosApiInstance.get("/users/me").then((res: AxiosResponse) => res.data),
+        enabled: !!token
+    })
+
+    if (isLoading) {
+        return (
+            <div className={"flex flex-col justify-center items-center h-screen bg-gradient-to-tr from-fuchsia-900 via-fuchsia-900 to-fuchsia-800"}>
+                <div className={"flex flex-col justify-center items-center opacity-30 w-1/2 h-1/2"}>
+                    <Lottie animationData={loading} />
+                </div>
+            </div>
+        )
+    }
+
+    return (
+        <Layout initialData={userData}>
+            <Toaster />
+            <Banner />
+            <main className={"flex flex-col items-center"}>
+                <div className={"container mx-auto max-w-7xl px-4 sm:px-6 lg:px-8"}>
+                    <div className={"flex flex-col justify-center bg-black bg-opacity-10 p-3 mb-8 rounded"}>
+                        <NextNProgress />
+                        <Component {...pageProps} />
+                        <FooterPerso />
+                    </div>
+                </div>
+            </main>
+        </Layout>
+    )
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,68 +1,15 @@
 import "@styles/globals.scss";
 import type { AppProps } from "next/app";
-import { Layout } from "@components/Layout";
-import Banner from "@components/Banner";
-import { FooterPerso } from "@components/FooterPerso";
-import { Toaster } from "react-hot-toast";
-import NextNProgress from "nextjs-progressbar";
-import { useBearStore } from "@store/useBearStore";
-import axiosApiInstance from "services/interceptorService";
-import { AxiosResponse } from "axios";
-import { Cookies } from "react-cookie";
-import { useEffect, useState } from "react";
-import Lottie from "lottie-react";
-import loading from "@assets/lottieJson/88944-vinyl-loading.json";
+import {QueryClient, QueryClientProvider, Hydrate} from "@tanstack/react-query";
+import {Exemple} from "./Exemple";
 
-export default function App({ Component, pageProps, data }: AppProps | any) {
-  const [isReady, setIsReady] = useState(false);
-
-  // token in cookie
-  const cookie = new Cookies();
-  const token = cookie.get("token");
-
-  useEffect(() => {
-    if (token) {
-      axiosApiInstance.get("/users/me").then((res: AxiosResponse) => {
-        useBearStore.setState({ user: res.data });
-        setIsReady(true);
-      });
-    }
-    setIsReady(true);
-  }, []);
-
-  return (
-    (isReady && (
-      <Layout initialData={data}>
-        <Toaster />
-        <Banner />
-        <main className={"flex flex-col items-center"}>
-          <div className={"container mx-auto max-w-7xl px-4 sm:px-6 lg:px-8"}>
-            <div
-              className={
-                "flex flex-col justify-center bg-black bg-opacity-10 p-3 mb-8 rounded"
-              }
-            >
-              <NextNProgress />
-              <Component {...pageProps} />
-              <FooterPerso />
-            </div>
-          </div>
-        </main>
-      </Layout>
-    )) || (
-      <div
-        className={
-          "flex flex-col justify-center items-center h-screen bg-gradient-to-tr from-fuchsia-900 via-fuchsia-900 to-fuchsia-800"
-        }
-      >
-        <div
-          className={
-            "flex flex-col justify-center items-center opacity-30 w-1/2 h-1/2"
-          }
-        >
-          <Lottie animationData={loading} />
-        </div>
-      </div>
+const queryClient = new QueryClient();
+export default function App({ Component, pageProps}: AppProps | any) {
+    return (
+        <QueryClientProvider client={queryClient}>
+            <Hydrate state={pageProps.dehydratedState}>
+                <Exemple {...pageProps} component={Component} />
+            </Hydrate>
+        </QueryClientProvider>
     )
-  );
 }
