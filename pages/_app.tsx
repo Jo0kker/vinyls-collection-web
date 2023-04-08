26bf53d64d09f1ab7e5d0659391e2b71c73b41ac
Jo0kker <boublou@hotmail.fr>
2023-04-08 22:21:26 +0200

    fix reactQuery install

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,24 @@
 import "@styles/globals.scss";
 import type { AppProps } from "next/app";
-import {QueryClient, QueryClientProvider, Hydrate} from "@tanstack/react-query";
-import {Exemple} from "./Exemple";
+import {
+  QueryClient,
+  QueryClientProvider,
+  Hydrate,
+} from "@tanstack/react-query";
+import { ProviderCustom } from "@components/ProviderCustom";
+// @ts-ignore
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
-export default function App({ Component, pageProps}: AppProps | any) {
-    return (
-        <QueryClientProvider client={queryClient}>
-            <Hydrate state={pageProps.dehydratedState}>
-                <Exemple {...pageProps} component={Component} />
-            </Hydrate>
-        </QueryClientProvider>
-    )
+export default function App({ Component, pageProps }: AppProps | any) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <ProviderCustom>
+          <Component {...pageProps} />
+        </ProviderCustom>
+      </Hydrate>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
 }
