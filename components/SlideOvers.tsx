43a1582c3f1bd791fd6a14ff522c926a8a4718e9
jonathan.francois <francois.jonathan13@gmail.com>
2023-07-20 07:49:24 +0200

    fix menu apparent quand slide over actif

--- components/SlideOvers.tsx
+++ components/SlideOvers.tsx
@@ -30,7 +30,7 @@ export default function SlideOvers ({
     return (
         <>
             <Transition.Root show={open} as={Fragment}>
-                <Dialog as="div" className="relative z-10" onClose={setOpen}>
+                <Dialog as="div" className="relative z-50" onClose={setOpen}>
                     <Transition.Child
                         as={Fragment}
                         enter="ease-in-out duration-500"
