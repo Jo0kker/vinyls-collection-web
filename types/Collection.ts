ca8c500cf2c28b7de80154b804d4bb2368916544
jonathan.francois <francois.jonathan13@gmail.com>
2023-07-20 07:46:37 +0200

    regroupement des types liés a une collection

--- types/Collection.ts
+++ types/Collection.ts
@@ -1,5 +1,7 @@
 import type { User } from './User';
+import type { Vinyl } from './Vinyl';
 
+// Info propre a la collection
 export interface Collection {
   id: number;
   name: string;
@@ -10,3 +12,52 @@ export interface Collection {
   updated_at: Date;
   user?: User;
 }
+
+// Info sur le contenu d'une collection de vinyl (collection utilisateur)
+export interface CollectionVinyl {
+    id: number;
+    collection_id: number;
+    vinyl_id: number;
+    format_vinyl_id: number;
+    cover_state: number;
+    year_purchased: string;
+    price: number;
+    is_sellable: number;
+    description: string;
+    created_at: Date;
+    updated_at: Date;
+    vinyl: Vinyl;
+    collection?: Collection;
+}
+
+// Info sur le contenu de la collection Search
+export interface Search {
+    id: number;
+    vinyl_id: number;
+    vinyl: Vinyl;
+    description: string;
+    //   discogs_id: number;
+    discogs?: object;
+    image: string;
+    user_id: number;
+    format_vinyl_id: number;
+    created_at: string;
+    updated_at: string;
+    user?: User;
+}
+
+// Info sur le contenu de la collection Trade
+export interface Trade {
+    id: number;
+    vinyl_id: number;
+    vinyl: Vinyl;
+    description: string;
+    image: string;
+    user_id: number;
+    format_vinyl_id: number;
+    created_at: string;
+    updated_at: string;
+    user?: User;
+}
+
+export type CollectionItem = CollectionVinyl | Search | Trade;
